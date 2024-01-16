1ac3f72b9842c23f677f86078e7e00360459a6a7
Lucas Pessoa <lucas.pessoa@bosal.com>
2024-01-16 15:33:49 -0300

    Carrinho.js, Produto.js, index.js e ContextoTema.js alterados

--- src/index.js
+++ src/index.js
@@ -8,11 +8,14 @@ import './estilizacaoTelas/index5.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import TemaProvider from './contexto/ContextoTema';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App/>
+    <TemaProvider>
+      <App/>
+    </TemaProvider>
   </BrowserRouter>
 );
 
