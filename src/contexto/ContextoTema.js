d1b5e40a41a3c547bec47d0cb4a51f9aac4b5d5c
Beatriz Silva Ramos <bibi_bagg@hotmail.com>
2024-01-25 13:17:04 -0300

    Sugestões de melhorias

--- src/contexto/ContextoTema.js
+++ src/contexto/ContextoTema.js
@@ -6,52 +6,58 @@ export const ContextoTema = createContext();
 const TemaProvider = ({children}) => {
 
     const [produtos, setProdutos] = useState([]);
+    const [total, setTotal] = useState({
+        quantidade: 0,
+        total: 0.00,
+    });
+    
+    const onChangeProducts = (novosProdutos) => {
+        
+        setTotal(novosProdutos.reduce(
+            (acumulador, item) => {
+                return {
+                    quantidade: acumulador.quantidade + item.quantidade,
+                    total: acumulador.total + item.preco * item.quantidade,
+                };
+            },
+            {
+                quantidade: 0,
+                total: 0.00,
+            }
+        ));
+    }
 
     const adicionarAoCarrinho = (novoProduto) => {
         const novosProdutos = [...produtos];
-        novosProdutos.push(novoProduto);
-        setProdutos(novosProdutos);
-    };
-
-    const AdicionarQuantia = (index) => {
-        const novosProdutos = [...produtos];
-        novosProdutos[index].quantidade++;
-
-        setProdutos(novosProdutos);
-    };
-
-    const RemoverQuantia = (index) => {
-        const novosProdutos = [...produtos];
-        novosProdutos[index].quantidade--;
-
-        setProdutos(novosProdutos);
-    };
-
-    const { total } = produtos.reduce(
-        (acumulador, item) => {
-            return {
-                quantidade: acumulador.quantidade + item.quantidade,
-                total: acumulador.total + item.preco * item.quantidade,
-            };
-        },
-        {
-            quantidade: 0,
-            total: 0.00,
+        let findProduct = produtos.findIndex(prod => prod.id == novoProduto.id);
+        if(findProduct != -1) {
+            novosProdutos[findProduct].quantidade++;
+        } else {
+            novosProdutos.push(novoProduto);
         }
-    );
-
-    const RemoverProduto = (index) => {
-        const novosProdutos = [...produtos];
-        novosProdutos.splice(index, 1);
+        
         setProdutos(novosProdutos);
+        onChangeProducts(novosProdutos);
+    };
+
+    const RemoverProduto = (index, full) => {
+        const novosProdutos = [...produtos];
+        if(novosProdutos[index].quantidade > 1 && !full) {
+            novosProdutos[index].quantidade--;
+        } else {
+            novosProdutos.splice(index, 1);
+        }
+        setProdutos(novosProdutos);
+        onChangeProducts(novosProdutos);
     };
 
     const LimparCarrinho = () => {
         setProdutos([]);
+        onChangeProducts([]);
     };
 
     return(
-        <ContextoTema.Provider value={{produtos, AdicionarQuantia, RemoverQuantia, total, RemoverProduto, LimparCarrinho, adicionarAoCarrinho}}>{children}</ContextoTema.Provider>
+        <ContextoTema.Provider value={{produtos, total, RemoverProduto, LimparCarrinho, adicionarAoCarrinho}}>{children}</ContextoTema.Provider>
     )
 }
 
