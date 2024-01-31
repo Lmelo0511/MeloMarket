7c5c22e6e230daec84270943ecbf06a552f17944
Lucas Melo <lucasmelo@MacBook-Air-de-Lucas.local>
2024-01-30 22:00:19 -0300

    atualizado api.js

--- src/servicos/api.js
+++ src/servicos/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // 13295230/json/
 
 const API = axios.create({
-    baseURL: "https://viacep.com.br/ws/"
+    baseURL: "https://viacep.com.br/ws/json"
 })
 
 export default API
